77953bc350d87e51aa505bdee1dd4be6d9c79794
Saroj Imger <esregmi@users.noreply.github.com>
2022-11-06 16:18:20 -0600

    update lab 3

--- lab3/Exercise2.js
+++ lab3/Exercise2.js
@@ -1,36 +1,38 @@
 class Subject {
     constructor() {
-        this.ObserverList = [];
+        this.observerlist = {};
     }
 
-    on(keys, fn) {
+    on(event, fn) {
+        if(this.observerlist[event]){
+            this.observerlist[event].push(fn);
 
-        this.ObserverList.push({ key: keys, fn: fn });
-    }
-
-    emit(key, msg) {
-        this.ObserverList.forEach(element => {
-
-            for (let k in element) {
-                // console.log('hello ',element['key']);
-                if (element['key'] === key) {
-                    console.log(msg);
-                    element['fn'](msg);
-                }
-            }
+        }else{
+            this.observerlist[event] =[fn];
         }
-
-        );
     }
 
+    emit(event, message){
+        if(this.observerlist[event]){
+            this.observerlist[event].forEach(element => {
+                element(message);
+                
+            });
+             
+        }
+       
+    }
 }
-
 const subject = new Subject();
+subject.on('eat', console.log); 
+subject.on('study', console.log); 
+
 function foo(msg) {
     console.log('foo: ' + msg);
 }
 subject.on('eat', foo);
 subject.on('study', foo);
-subject.emit('eat', 'Corn');
-subject.emit('study', 'cs445');
 
+subject.emit('eat', 'Corn');
+
+subject.emit('study', 'cs445');
